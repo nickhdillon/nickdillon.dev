6d2dd0ec6eabf75423ad7202f9f6d47ca09f31b6
Nick Dillon <nickhds@gmail.com>
2025-03-06 23:59:50 -0600

    Update logo and favicon

--- resources/views/components/portfolio/header.blade.php
+++ resources/views/components/portfolio/header.blade.php
@@ -1,10 +1,7 @@
 <div class="fixed w-full z-10 px-3.5 py-3 sm:py-3.5 border-b sm:px-10 border-slate-700 backdrop-blur-2xl text-slate-50">
     <nav class="flex items-center justify-between mx-auto xl:max-w-5xl">
-        <a href="{{ route('portfolio') }}" wire:navigate>
-            <h1
-                class="text-3xl font-semibold tracking-tighter uppercase duration-300 ease-in-out hover:text-pink-500 hover:scale-110">
-                Nick Dillon
-            </h1>
+        <a href="{{ route('portfolio') }}" wire:navigate class="hover:scale-110">
+            <img src="{{ asset('logo.svg') }}" class="w-full duration-300 ease-in-out size-7 hover:scale-110" />
         </a>
 
         @livewire('portfolio.socials')
