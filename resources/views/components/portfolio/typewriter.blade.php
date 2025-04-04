43c7fbc7395e88f17797a2446ebb9f3d258e0563
Nick Dillon <nickhds@gmail.com>
2025-04-04 08:42:25 -0500

    Change title

--- resources/views/components/portfolio/typewriter.blade.php
+++ resources/views/components/portfolio/typewriter.blade.php
@@ -7,7 +7,7 @@
         'Docker.'
     ]
 }" class="w-9/12 text-xl font-semibold text-slate-50 sm:w-full">
-    I'm a full stack developer, working with
+    I'm a software developer, working with
 
     <span x-typewriter.5s.cursor="technologies"></span>
 </div>
