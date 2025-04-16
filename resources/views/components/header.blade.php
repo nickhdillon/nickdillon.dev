4f8560e797264bb37f6151b36cef430a44a4eb2a
Nick Dillon <nickhds@gmail.com>
2025-04-16 12:14:51 -0500

    Change top value for mobile

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,4 +1,4 @@
-<div class="sticky top-5 w-11/12 max-w-6xl mx-auto shadow-lg z-10 p-3 sm:p-4 border rounded-[12px] border-white/15 backdrop-blur-md text-zinc-50">
+<div class="sticky top-4 sm:top-5 w-11/12 max-w-6xl mx-auto shadow-lg z-10 p-3 sm:p-4 border rounded-[12px] border-white/15 backdrop-blur-md text-zinc-50">
     <nav class="flex items-center justify-between">
         <a href="{{ route('home') }}" wire:navigate>
             <img src="{{ asset('logo.svg') }}" class="w-10 duration-300 ease-in-out hover:scale-110" />
