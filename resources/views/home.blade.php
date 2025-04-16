05d565fb8298524e7ccf01ebcfd694995ac0a607
Nick Dillon <nickhds@gmail.com>
2025-04-16 15:42:14 -0500

    Remove dark class attribute

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="dark">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -15,7 +15,7 @@
         @livewireStyles
     </head>
 
-    <body x-init="() => document.documentElement.classList.add('dark')" class="min-h-screen bg-zinc-900 bg-grid bg-repeat text-zinc-100">
+    <body class="min-h-screen bg-zinc-900 bg-grid bg-repeat text-zinc-100">
         <x-header />
 
         <x-intro />
