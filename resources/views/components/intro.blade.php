88c04f64e476c0daf0e8cab1a373b066b356e27b
Nick Dillon <nickhds@gmail.com>
2025-04-16 22:58:29 -0500

    Add pulsing background

--- resources/views/components/intro.blade.php
+++ resources/views/components/intro.blade.php
@@ -2,7 +2,10 @@
     <div class="relative flex flex-col items-center max-w-xl mx-auto space-y-4 text-center group sm:px-4">
         <div class="z-0 flex flex-col items-center space-y-4">
             <img src="{{ asset('profile.jpg') }}"
-                class="w-64 h-64 duration-500 ease-in-out rounded-full sm:w-56 sm:h-56 shadow-3xl" />
+                class="w-64 h-64 duration-500 ease-in-out rounded-full sm:w-56 sm:h-56 shadow-3xl relative" />
+
+            <div class="absolute -top-4 blur-3xl h-90 w-90 rounded-full bg-radial from-orange-500/25 to-transparent pointer-events-none z-[-1] animate-[pulse_3s_ease-in-out_infinite]">
+            </div>
 
             <h1 class="italic">
                 <div
--- resources/views/components/typewriter.blade.php
+++ resources/views/components/typewriter.blade.php
@@ -6,7 +6,7 @@
         'Inertia.js.',
         'Docker.'
     ]
-}" class="w-9/12 text-xl font-medium text-slate-50 sm:w-full">
+}" class="w-9/12 text-xl font-medium text-zinc-50 sm:w-full">
     I'm a software developer, working with
 
     <span x-typewriter.5s.cursor="technologies"></span>
