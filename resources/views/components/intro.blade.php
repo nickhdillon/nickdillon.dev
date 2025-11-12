bc581dfb6e390d1ad7fd09fc17418f04f2aaae8e
Nick Dillon <nickhds@gmail.com>
2025-11-12 16:50:52 -0600

    style: change theme to red

--- resources/views/components/intro.blade.php
+++ resources/views/components/intro.blade.php
@@ -4,18 +4,18 @@
             <img src="{{ asset('profile.jpg') }}"
                 class="w-64 h-64 duration-500 ease-in-out rounded-full sm:w-56 sm:h-56 shadow-3xl relative" />
 
-            <div class="absolute -top-4 blur-3xl h-90 w-90 rounded-full bg-radial from-orange-500/25 to-transparent pointer-events-none z-[-1] animate-[pulse_3s_ease-in-out_infinite]">
+            <div class="absolute -top-4 blur-3xl h-90 w-90 rounded-full bg-radial from-red-500/25 to-transparent pointer-events-none z-[-1] animate-[pulse_3s_ease-in-out_infinite]">
             </div>
 
-            <h1 class="italic">
+            <h1>
                 <div
                     class="relative items-center justify-center inline-block w-auto h-auto px-1 mb-2 text-base font-medium text-opacity-100 transition-all duration-300 rounded outline-none group active:ring-0 active:outline-none">
-                    <span class="relative z-20 p-1 text-5xl">
+                    <span class="relative italic z-20 p-1 text-5xl">
                         Hi, I'm Nick!
                     </span>
 
                     <span
-                        class="absolute bottom-0 left-0 z-10 w-full h-3 transition-all duration-300 ease-out -skew-x-12 bg-accent">
+                        class="absolute bottom-0 left-0 z-10 w-full h-3 transition-all duration-300 ease-out -skew-x-13 bg-accent">
                     </span>
                 </div>
             </h1>
